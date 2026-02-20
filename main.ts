fd3105532ef5c99cb630b5e2d4add5d9f7a3ea45
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-20 13:20:36 -0500

    Created a Tempurate sensor and Changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Feb 2026
+ * This program 
 */
 
-basic.showString('Hello, World!')
+// Variable for the Temperature
+let temperature: number
+
+//The Setup for the smilely face
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+//when A is pressed it will show the temperature 
+input.onButtonPressed(Button.A, function () {
+  temperature = input.temperature() 
+  basic.showString('The temperature is' + temperature)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+  basic.pause(1000)
+})
